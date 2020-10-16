eb338d94b5408023eaf2d81484ce991b089c4186
Shravan Patel <2000shravan@gmail.com>
2020-10-16 20:17:10 +0530

    no change

--- routes/auth.js
+++ routes/auth.js
@@ -15,7 +15,7 @@ const {SENDGRID_API, EMAIL} = require('../config/keys')
 const transporter = nodemailer.createTransport(sendgridTransport({
     auth:{
         api_key:SENDGRID_API    
-    }
+    } 
 }))
 
 router.post('/signup', (req, res) => {
